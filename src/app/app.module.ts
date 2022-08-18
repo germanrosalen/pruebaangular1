3d672814c621e3841e00225110af5e68db53f655
germanrosalen <101280898+germanrosalen@users.noreply.github.com>
2022-08-18 00:49:19 -0300

    creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillComponent } from './componentes/skill/skill.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { PiedepaginaComponent } from './componentes/piedepagina/piedepagina.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillComponent,
+    ProyectosComponent,
+    PiedepaginaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
